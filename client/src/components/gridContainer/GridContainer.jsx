1ceb9485ad623f530830bbfa58ce3a1e8f44e0ac
Matteo <matteotedesco@outlook.com>
2023-06-13 12:56:27 +0200

    added layout and gridContainer component

---
+++ client/src/components/gridContainer/GridContainer.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react"
+import { Col, Container, Row } from "react-bootstrap"
+import "./index.css"
+
+export default function GridContainer({ cols, maxCols, children }) {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+  const [maxTabCols, setMaxTabCols] = useState(maxCols / 2)
+  const [maxRows, setMaxRows] = useState(cols)
+
+  // Update the windowWidth state when the window is resized
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth)
+    window.addEventListener("resize", handleResize)
+    if (windowWidth < 576) {
+      setMaxTabCols(1)
+      setMaxRows(Math.ceil(cols / maxTabCols))
+    } else if (windowWidth > 575 && windowWidth < 991) {
+      setMaxTabCols(2)
+      setMaxRows(Math.ceil(cols / maxTabCols))
+    } else if (windowWidth >= 991 && windowWidth < 1201) {
+      setMaxTabCols(3)
+      setMaxRows(Math.ceil(cols / maxTabCols))
+    } else {
+      setMaxTabCols(maxCols)
+      setMaxRows(Math.ceil(cols / maxTabCols))
+    }
+    return () => window.removeEventListener("resize", handleResize)
+  }, [windowWidth, maxCols, cols, maxTabCols])
+
+  const generateRows = () => {
+    const rows = []
+    for (let i = 0; i < maxRows; i++) {
+      rows.push(
+        <Row key={`row-${i}`} className="justify-content-center" as="article">
+          {children.slice(i * maxTabCols, (i + 1) * maxTabCols).map((item, i) => (
+            <Col key={`row-${i}-col-${i}`} xs="auto" md={6} lg="auto" xl={3} as="section">
+              {item}
+            </Col>
+          ))}
+        </Row>
+      )
+    }
+    return rows
+  }
+
+  return <Container className="grid-wrapper grid-wrapper-row">{generateRows()}</Container>
+}
---
+++ client/src/components/layout/Layout.jsx
@@ -0,0 +1,37 @@
+import { IconHome2, IconLogin } from "@tabler/icons-react"
+import { Navbar as BootstrapNavbar, Container, Nav } from "react-bootstrap"
+import "./index.css"
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  )
+}
+
+function Navbar() {
+  const links = [
+    { title: "Home", icon: IconHome2 },
+    { title: "Login", icon: IconLogin },
+  ]
+  return (
+    <BootstrapNavbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
+      <Container>
+        <BootstrapNavbar.Brand>CMSmall</BootstrapNavbar.Brand>
+        <BootstrapNavbar.Toggle aria-controls="responsive-navbar-nav" />
+        <BootstrapNavbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto">
+            {links.map((link, index) => (
+              <Nav.Link key={index} href={`/${link.title.toLowerCase()}`} className="link">
+                {<link.icon size={20} />}
+                <p>{link.title}</p>
+              </Nav.Link>
+            ))}
+          </Nav>
+        </BootstrapNavbar.Collapse>
+      </Container>
+    </BootstrapNavbar>
+  )
+}
